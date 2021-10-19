218f200f60941b11a4021545888254b13cad472e
lverghese <loganverghese@gmail.com>
2021-10-19 14:03:44 -0400

    index to export routes done

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,28 +4,29 @@ const userController = {
     //get all users
     getAllUser(req, res) {
         User.find({})
-      .populate({
-        path: 'thoughts',
-        select: '-__v'
-      })
       .select('-__v')
       .sort({ _id: -1 })
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
-        res.sendStatus(400);
+        res.sendStatus(400).json(err);
       });
     },
 
     //get one user by id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-      .populate({
-        path: 'thoughts',
-        select: '-__v'
-      })
+      .populate(
+          { path: 'thoughts', select: '__v' },
+        {path: 'friends', select: '-__v' })
       .select('-__v')
-      .then(dbUserData => res.json(dbUserData))
+      .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+          }
+          res.json(dbUserData)
+      })
       .catch(err => {
         console.log(err);
         res.sendStatus(400);
@@ -44,7 +45,7 @@ const userController = {
         User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then(dbUserData => {
         if (!dbUserData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+          res.status(404).json({ message: 'No user found with this id!' });
           return;
         }
         res.json(dbUserData);
